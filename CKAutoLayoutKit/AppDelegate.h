de18af6f8eb9c8364d2c2d046f6ead15c28d2c9f
mac <mac@macdeMac-mini.local>
2014-09-28 14:56:05 +0800

    Initial Commit

---
+++ CKAutoLayoutKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CKAutoLayoutKit
+//
+//  Created by mac on 14-9-28.
+//  Copyright (c) 2014年 kaicheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CKAutoLayoutKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CKAutoLayoutKit
+//
+//  Created by mac on 14-9-28.
+//  Copyright (c) 2014年 kaicheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
